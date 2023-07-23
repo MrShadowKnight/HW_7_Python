278d9f2134e8b0d9ef781dcc0b202aec97b91e4e
MrShadowKnight <138589809+MrShadowKnight@users.noreply.github.com>
2023-07-23 11:23:40 +0300

    Add.changes

--- main.py
+++ main.py
@@ -82,3 +82,99 @@
 # print(is_palindrome(123321))  
 # print(is_palindrome(546645))  
 # print(is_palindrome(421987))  
+
+# Поліндром (слово)
+
+# def isPolidrom(word):
+#     revers_word = word[::-1]
+#     if revers_word == word:
+#         return True
+#     else:
+#         return False
+    
+# isPolidrom("rer")
+
+# Завдання зі списком
+
+# def square_list(numbers):
+#     squared_numbers = []
+
+#     for num in numbers:
+#         squared_numbers.append(num * num)
+    
+#     return squared_numbers
+
+# main_list = [1, 4, 7, 65, 34, 56, 78, 9]
+# print(square_list(main_list))
+
+# Факторіал числа
+
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
+
+# print(factorial(4))
+
+# Конвертація римських чисел
+
+# def roman_to_arabic(roman_numeral):
+#     roman_dict = {
+#         'I': 1,
+#         'V': 5,
+#         'X': 10,
+#         'L': 50,
+#         'C': 100,
+#         'D': 500,
+#         'M': 1000
+#     }
+
+#     arabic_num = 0
+#     prev_value = 0
+
+#     for symbol in reversed(roman_numeral):
+#         value = roman_dict[symbol]
+
+#         if value < prev_value:
+#             arabic_num -= value
+#         else:
+#             arabic_num += value
+
+#         prev_value = value
+
+#     return arabic_num
+
+# print(roman_to_arabic("MCIL"))
+
+# Підрахунок слів
+
+# def count_words(sentence):
+#     words = sentence.split()
+#     return len(words)
+
+# text = "Просто текст для перевірки роботи"
+# print(count_words(text))
+
+# Шифр цезаря
+
+import json
+
+def casarCode(txt, key=3):
+    with open("alphabet.json", "r") as file:
+        alphabet_data = json.load(file)
+    code_in_number = []
+    for letter in txt:
+        for l in alphabet_data:
+            if alphabet_data[f"{l}"] == letter.lower():
+                code_in_number.append(int(l) + int(key))
+         
+    code_word = ''
+    for num in code_in_number:
+        if num <= 26:    
+            code_word += alphabet_data[f"{num}"]
+        else:
+            code_word += alphabet_data[f"{num - 26}"]
+    return code_word.capitalize
+
+print(casarCode("aple"))
